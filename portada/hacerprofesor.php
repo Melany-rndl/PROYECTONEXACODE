96eb9b509f83ee8243f3facb883619d9bb2778f2
Melany-rndl <valerasanchezmelany@pedropoveda.edu.bo>
2025-09-09 20:11:37 -0400

    haciendo profes

---
+++ portada/hacerprofesor.php
@@ -0,0 +1,10 @@
+<?php
+$id_cuenta=$_GET['ci'];
+
+$sql = "UPDATE cuenta SET rol='profesor' WHERE id_cuenta='$id_cuenta'";
+if (mysqli_query($conn, $sql)){
+    header("Location: administrador.php");
+
+}else{
+    echo"Error:" .mysqli_error($conn);
+}
