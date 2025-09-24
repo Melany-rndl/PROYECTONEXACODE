304ed41ff8a2b91d3c0bd7264993290e6fba5dd3
Melany-rndl <valerasanchezmelany@pedropoveda.edu.bo>
2025-09-24 14:50:30 -0400

    error

--- Guardar-Tarea.php
+++ Guardar-Tarea.php
@@ -1,39 +1,24 @@
-<?php
-$servername = "localhost";
-$username   = "root";     
-$password   = "";        
-$database   = "p25";      
-
-$conn = mysqli_connect($servername, $username, $password, $database);
-
-if (!$conn) {
-    die("Conexión fallida: " . mysqli_connect_error());
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $titulo = mysqli_real_escape_string($conn, $_POST["titulo"]);
-    $descripcion = mysqli_real_escape_string($conn, $_POST["descripcion"]);
-    $tema = mysqli_real_escape_string($conn, $_POST["tema"]);
-    $nota = !empty($_POST["nota"]) ? $_POST["nota"] : "NULL";
-    $clase_id = $_POST["clase_id_clase"];
-
-    if (!empty($titulo) && !empty($descripcion) && !empty($clase_id)) {
-        $sql = "INSERT INTO tarea (titulo, descripcion, tema, nota, clase_id_clase) 
-                VALUES ('$titulo', '$descripcion', " . 
-                (!empty($tema) ? "'$tema'" : "NULL") . ", " . 
-                ($nota !== "NULL" ? "'$nota'" : "NULL") . ", '$clase_id')";
-
-        if (mysqli_query($conn, $sql)) {
-            echo "<h2>Tarea guardada con éxito</h2>";
-            echo "<a href='Tareas-Formulario.php'>Crear otra tarea</a><br>";
-            echo "<a href='Lista-Tareas.php'>Ver lista de tareas</a>";
-        } else {
-            echo "Error: " . mysqli_error($conn);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .nombre{
+        color:blue ;
+    animation:mel 10s}
+        @keyframes mel{
+            30% {color:pink;}
+            60%{color:black;}
         }
-    } else {
-        echo "Título, descripción y clase son obligatorios.";
-    }
-}
-
-mysqli_close($conn);
-?>
+    </style>
+</head>
+<body>
+    <div name="nombre"><h1>MELANY</h1></div>
+    <form action="parte2"method="post">
+    <label>texto</label>
+    <input type="number" name="numero" id="">
+</form>
+</body>
+</html>
---
+++ parte2.php
@@ -0,0 +1,2 @@
+<?php
+$nota=$_POST
