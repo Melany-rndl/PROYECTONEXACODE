0cc58f6e36a9459511999a4980de7e0ef5a6725c
Melany-rndl <valerasanchezmelany@pedropoveda.edu.bo>
2025-09-23 23:02:07 -0400

    ya funciono

--- portada/cambiarrol.php
+++ portada/cambiarrol.php
@@ -5,7 +5,7 @@ $ci = $_GET['ci'];
 $rol = $_GET['rol'];
 
 // Evitamos que el admin sea modificado
-$sql = "UPDATE cuenta SET rol = '$rol' WHERE id_cuenta = (SELECT id_cuenta FROM informacion WHERE ci='$ci' LIMIT 1 )AND rol != 'admin'";
+$sql = "UPDATE cuenta SET rol = '$rol' WHERE id_cuenta = (SELECT cuenta_id_cuenta FROM informacion WHERE ci='$ci' LIMIT 1 )AND rol != 'admin'";
 
 if (mysqli_query($conn, $sql)) {
     header("Location: admin.php");
--- portada/verbloqueo.php
+++ portada/verbloqueo.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <style>
+        h2{
+            background: yellowgreen;
+            color:red;
+            font-size:30px;
+        }
+    </style>
 </head>
 <body>
-    <h2 style="color:red; text-align:center;">
+    <h2>
         LO SENTIMOS MUCHO, TU CUENTA ESTÁ BLOQUEADA
     </h2>
 </body>
